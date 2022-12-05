8e348e22aad884d8eab3cca6d8c56b1e9d4e19a8
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-05 15:39:32 +0530

    home commit

--- src/main/java/com/example/employee_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employee_backend/controller/EmployeeController.java
@@ -1,8 +1,17 @@
 package com.example.employee_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class EmployeeController {
+
+    @GetMapping("/")
+
+    public String Homepage(){
+
+        return("Welcome Employee");
+
+    }
 }
