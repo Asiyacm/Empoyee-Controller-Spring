c311038f8e76652c9161e1184137fa934b2e1b3a
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-06 15:08:26 +0530

    insert commit

--- src/main/java/com/example/employee_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employee_backend/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.example.employee_backend.controller;
 
+import com.example.employee_backend.dao.EmployeesDao;
 import com.example.employee_backend.model.Employees;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class EmployeeController {
+
+    @Autowired
+    private EmployeesDao dao;
 
     @GetMapping("/")
 
@@ -21,6 +26,7 @@ public class EmployeeController {
     @PostMapping(path= "/add",consumes = "application/json",produces = "application/json")
     public String AddEmployee(@RequestBody Employees e){
         System.out.println(e.getName().toString());
+        dao.save(e);
         return"Employee Added Sucessfully";
     }
 
