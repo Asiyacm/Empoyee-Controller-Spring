e383593f5e092f18de04caff19779004b212d3b0
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-08 12:27:15 +0530

    delete commit

--- src/main/java/com/example/employee_backend/dao/EmployeeDao.java
+++ src/main/java/com/example/employee_backend/dao/EmployeeDao.java
@@ -1,7 +1,9 @@
 package com.example.employee_backend.dao;
 
 import com.example.employee_backend.Model.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -13,4 +15,9 @@ public interface EmployeeDao extends CrudRepository<Employee,Integer> {
 
 @Query(value = "SELECT `id`, `companyname`, `designation`, `empcode`, `empname`, `mobile`, `password`, `salary`, `username` FROM `employee` WHERE `empcode`=:empcode",nativeQuery = true)
 List<Employee> Search(@Param("empcode") Integer empcode);
+
+@Modifying
+@Transactional
+    @Query(value ="DELETE FROM `employee` WHERE `id`=:id",nativeQuery = true)
+    void deleteEmployee(@Param("id")Integer id);
 }
